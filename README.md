27d77ca78118ab13624e1044f97d596dd8e96899
Leoteixeira3010 <leonardoteixeiraesilva@gmail.com>
2025-07-17 10:40:47 -0300

    📦 Projeto file-integrity-watcher - versão inicial

---
+++ README.md
@@ -0,0 +1,10 @@
+﻿# File Integrity Watcher
+
+Monitora alterações de arquivos em tempo real.
+
+## Funcionalidades:
+- Watchdog para eventos de arquivos
+- Geração de logs de alteração
+- Pode ser usado para detectar ransomwares
+
+Stack: Python + watchdog
---
+++ watcher.py
@@ -0,0 +1,22 @@
+﻿from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
+
+class MonitorHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        print(f"Arquivo modificado: {event.src_path}")
+    def on_created(self, event):
+        print(f"Novo arquivo: {event.src_path}")
+
+if __name__ == "__main__":
+    path = "."
+    event_handler = MonitorHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
